cf4cb5bdd6ec836d070aacb51cec3876b8374c6e
m.trauernicht <m.trauernicht@nki.nl>
2021-03-12 15:00:03 +0100

    added analysis from pMT02 in K562

---
+++ gen-1_K562/raw-data-analysis/config.py
@@ -0,0 +1,18 @@
+from os.path import join
+
+# Globals ---------------------------------------------------------------------
+
+# Full path to working directory
+W_DIR = '/DATA/usr/m.trauernicht/projects/SuRE-TF/data/gcf6371_K562/'
+
+# Expression and copy number (ECN)---------------------------------------------
+
+# Full path to cDNA and pDNA raw data folder (fastq)
+ECN_DIR = W_DIR + 'raw/'
+
+# Extract cDNA and pDNA sample names
+ECN, = glob_wildcards(join(ECN_DIR, '{ecn,[^/]+}_R1_001.fastq.gz'))
+print(ECN)
+
+# Pattern for SE read
+S1 = '{ecn}_R1_001.fastq.gz'
