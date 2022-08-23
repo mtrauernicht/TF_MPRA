b642dd0c2b421b45b78fc990b0575297584a344d
mtrauernicht <m.trauernicht@nki.nl>
2022-08-23 09:29:28 +0200

    updated gitignore

--- analyses/gen-1_stimulation-2/raw-data_analysis/config.py
+++ analyses/gen-1_stimulation-2/raw-data_analysis/config.py
@@ -3,12 +3,12 @@ from os.path import join
 # Globals ---------------------------------------------------------------------
 
 # Full path to working directory
-W_DIR = '/DATA/usr/m.trauernicht/projects/tf_activity_reporter/data/SuRE_TF_2/'
+W_DIR = '/shared/gcf/m.trauernicht/6139/'
 
 # Expression and copy number (ECN)---------------------------------------------
 
 # Full path to cDNA and pDNA raw data folder (fastq)
-ECN_DIR = W_DIR + 'raw/fastq_files/'
+ECN_DIR = W_DIR + 'fastq_files/'
 
 # Extract cDNA and pDNA sample names
 ECN, = glob_wildcards(join(ECN_DIR, '{ecn,[^/]+}_R1_001.fastq.gz'))
